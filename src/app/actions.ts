50b2597fd83894b7b774bcbdc872f081a011a348
Nicolas Zilli <m.nicolas.zilli@gmail.com>
2024-09-26 12:32:09 +0200

    add turnstile

---
+++ src/app/actions.ts
@@ -0,0 +1,81 @@
+"use server";
+
+import { getRequestContext } from "@cloudflare/next-on-pages";
+import { headers } from "next/headers";
+
+export type SendContactFormState = {
+  success: number;
+  error?: string;
+};
+
+export async function sendContactForm(
+  prevState: SendContactFormState,
+  formData: FormData
+): Promise<SendContactFormState> {
+  try {
+    const rawFormData = {
+      firstName: formData.get("first-name"),
+      lastName: formData.get("last-name"),
+      email: formData.get("email"),
+      message: formData.get("message"),
+    };
+
+    if (prevState.success >= 3) {
+      throw new Error("Too many submissions.");
+    }
+
+    if (typeof rawFormData.email !== "string") {
+      throw new Error("Email is invalid.");
+    }
+
+    await verifyTurnstile(formData.get("cf-turnstile-response"));
+
+    const myKv = getRequestContext().env.CONTACT;
+
+    await myKv.put(rawFormData.email, JSON.stringify(rawFormData));
+
+    return { success: prevState.success + 1, error: undefined };
+  } catch (e) {
+    if (e instanceof Error) {
+      return { ...prevState, error: e.message };
+    }
+    return { ...prevState, error: "An unknown error has occurred, try again." };
+  }
+}
+
+async function verifyTurnstile(cfTurnstileResponse: FormDataEntryValue | null) {
+  if (!cfTurnstileResponse) {
+    throw new Error("Turnstile is missing.");
+  }
+  const ip = headers().get("X-Forwarded-For");
+
+  if (!ip) {
+    throw new Error("Can't verify ip.");
+  }
+
+  if (!process.env.NEXT_PRIVATE_TURNSTILE_SECRET_KEY) {
+    throw new Error("Turnstile not configured.");
+  }
+
+  const formData = new FormData();
+  formData.append("secret", process.env.NEXT_PRIVATE_TURNSTILE_SECRET_KEY);
+  formData.append("response", cfTurnstileResponse);
+  formData.append("remoteip", ip);
+
+  const url = "https://challenges.cloudflare.com/turnstile/v0/siteverify";
+
+  try {
+    const result = await fetch(url, {
+      body: formData,
+      method: "POST",
+    });
+
+    const outcome = await result.json<{ success: boolean }>();
+
+    if (!outcome.success) {
+      throw Error("Failed to verify Turnstile.");
+    }
+  } catch {
+    throw Error("Failed to verify Turnstile.");
+  }
+}
---
+++ src/app/contact/form.tsx
@@ -0,0 +1,156 @@
+"use client";
+
+import { sendContactForm, SendContactFormState } from "@/app/actions";
+import { Button } from "@/components/button";
+import { Field, Input, Label, Textarea } from "@headlessui/react";
+import clsx from "clsx";
+import Script from "next/script";
+import { useEffect } from "react";
+import { useFormState, useFormStatus } from "react-dom";
+
+const turnstileClass = "cf-turnstile";
+
+const initialState: SendContactFormState = {
+  success: 0,
+};
+
+export function ContactForm() {
+  const [{ success, error }, formAction] = useFormState(
+    sendContactForm,
+    initialState
+  );
+
+  return (
+    <form action={formAction} className="px-6 lg:px-8">
+      <div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+          <Field>
+            <Label
+              htmlFor="first-name"
+              className="block text-sm font-semibold leading-6  text-zinc-600 dark:text-zinc-400"
+            >
+              First name
+            </Label>
+            <div className="mt-2.5">
+              <Input
+                id="first-name"
+                name="first-name"
+                type="text"
+                autoComplete="given-name"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-zinc-600 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </Field>
+          <Field>
+            <Label
+              htmlFor="last-name"
+              className="block text-sm font-semibold leading-6 text-zinc-600 dark:text-zinc-400"
+            >
+              Last name
+            </Label>
+            <div className="mt-2.5">
+              <Input
+                id="last-name"
+                name="last-name"
+                type="text"
+                autoComplete="family-name"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-zinc-600 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </Field>
+          <Field className="sm:col-span-2">
+            <Label
+              htmlFor="email"
+              className="block text-sm font-semibold leading-6 text-zinc-600 dark:text-zinc-400"
+            >
+              Email
+            </Label>
+            <div className="mt-2.5">
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-zinc-600 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </Field>
+          <Field className="sm:col-span-2">
+            <Label
+              htmlFor="message"
+              className="block text-sm font-semibold leading-6 text-zinc-600 dark:text-zinc-400"
+            >
+              Message
+            </Label>
+            <div className="mt-2.5">
+              <Textarea
+                id="message"
+                name="message"
+                rows={4}
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-zinc-600 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
+                defaultValue={""}
+              />
+            </div>
+          </Field>
+          <div className="sm:col-span-2">
+            <Turnstile />
+          </div>
+        </div>
+        <div className="mt-8 flex flex-col md:flex-row justify-end items-center gap-4">
+          {(success ?? 0) > 0 && (
+            <p className="text-base text-zinc-600 dark:text-zinc-400 mr-auto">
+              Message sent successfully{success > 1 && ` (${success})`}
+            </p>
+          )}
+          <Submit />
+        </div>
+
+        {error && (
+          <p className="text-base text-red-600 dark:text-red-400">{error}</p>
+        )}
+      </div>
+    </form>
+  );
+}
+
+function Submit() {
+  const { pending } = useFormStatus();
+
+  useEffect(() => {
+    if (pending) {
+      // @ts-ignore
+      window.turnstile.reset(".cf-turnstile");
+    }
+  }, [pending]);
+
+  return (
+    <Button
+      variant="primary"
+      type="submit"
+      disabled={pending}
+      className={clsx(
+        "rounded-md bg-teal-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600 disabled:pointer-events-none select-none"
+      )}
+    >
+      {pending ? "Sending..." : "Send message"}
+    </Button>
+  );
+}
+
+function Turnstile() {
+  return (
+    <>
+      <Script
+        src="https://challenges.cloudflare.com/turnstile/v0/api.js"
+        async
+        defer
+      ></Script>
+      <div
+        className={turnstileClass}
+        data-sitekey={process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY}
+        data-size="flexible"
+        data-callback="javascriptCallback"
+      ></div>
+    </>
+  );
+}
